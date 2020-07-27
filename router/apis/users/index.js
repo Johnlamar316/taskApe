7e757667875861081e849162d19ff8cfb99cf66c
John Lamar <okaforjohnekene316@gmail.com>
2020-07-27 11:42:43 +0100

    login api

--- router/apis/users/index.js
+++ router/apis/users/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const router = express.Router();
 
@@ -9,6 +10,8 @@ const validateLoginInput = require("../../../validation/login");
 
 //load User model
 const User = require("../../../models/Users");
+
+const secrets = require("../../../config/keys").secretOrKey;
 
 //test route
 router.get('/test', (req, res) => res.send('Users Page'));
@@ -83,6 +86,48 @@ router.post('/login', (req,res) => {
     if (!isValid) {
         return res.status(400).json(getErrors);
     }
+    const username = req.body.username;
+    const password = req.body.password;
+    //find user by username
+    User.findOne({ username }).then((user) => {
+        //check for user
+        if (!user) {
+            return res.status(404).json({
+                status: false,
+                message: `User not found`,
+            });
+        }
+        // check password
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if(isMatch) {
+                //user matched
+                // create jwt payload
+                const payload = { id: user.id, username: user.username, avatar: user.avatar };
+                //sign in Token
+                jwt.sign(payload, secrets, {expires: 3600}, (err, token) => {
+                    if(err) throw err;
+                    res.json({
+                        _meta: {
+                            status: true,
+                            message: `Welcome back ${user.username}`,
+                            token: "Bearer " + token //authenticating bearer
+                        },
+                        data: {
+                            id: user.id,
+                            username: user.username,
+                            email: user.email,
+                            date: user.date,
+                        },
+                    });
+                })
+            } else {
+                return res.status(400).json({
+                    status: false,
+                    message: "password not found",
+                });
+            }
+        })
+    })
 })
 
 
